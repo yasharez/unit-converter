17b276767153e0e45163daf4465bfe8054db0b93
Yashar Zafari <yasharez@gmail.com>
2023-01-25 20:59:10 -0800

    Progress save

---
+++ main.py
@@ -0,0 +1,13 @@
+# Yashar Zafari
+# GitHub: yasharez
+# Date: 01/25/2023
+# Main file for unit-converter program
+
+# Import libraries
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.title("Unit Converter")
+
+root.mainloop()
